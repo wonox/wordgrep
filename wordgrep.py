51d110e639b44fd02fc6832ed3f74f79b36c4e5a
wonox <12597445+wonox@users.noreply.github.com>
2022-11-25 17:36:54 +0900

    3rd commit

--- wordgrep.py
+++ wordgrep.py
@@ -1,40 +1,57 @@
-# Wordファイルからのテキスト抽出
+# Text extraction from Word(docx) files
 from docx import Document
 import sys
+import argparse
 import os
 import re
 
+# outline --help
+parser = argparse.ArgumentParser(
+    description="Text extraction from Word(docx) files")
+
+parser.add_argument(
+    '-f',
+    '--file',
+    type=str,
+    help='path\\filename.docx',
+    required=True)
+
+parser.add_argument(
+    '-w',
+    '--word',
+    type=str,
+    help='search word(Regular expression enabled)',
+    required=True)
+
+cmd_args = parser.parse_args()
+file_path = cmd_args.file
+
+
 def wordgrep(path, search_word):
     lis = []
-    # document = Document(r'C:\Users\libso23-user\OneDrive - The University of Tokyo\ドキュメント\korekara_doc20221101-1.docx')
     document = Document(path)
     # 1パラグラフごとに配列に入れる
     for i, p in enumerate(document.paragraphs):
-        # print(i, p.text)
-        i_text = str(i) + ' ｜ ' + p.text
+        i_text = str(i) + ' : ' + p.text
         lis.append(i_text)
-    # print(lis)
     # 検索条件
-    # pattern = ".*DOI.*"
-    pattern = ".*" + search_word +".*"
+    pattern = ".*" + search_word + ".*"
 
     # Match the above pattern using list comprehension
-    filtered_lis = [x for x in lis if re.match(pattern, x)]
+    # filtered_lis = [x for x in lis if re.match(pattern, x)]
+    filtered_lis = [x for x in lis if re.fullmatch(pattern, x, re.IGNORECASE)]
 
     # 結果の配列の表示
-    print(filtered_lis)
+    # print(filtered_lis)
+    print('\n'.join(filtered_lis))
 
-    # path_w = 'test_w.txt'
-    # with open(path_w, mode='w') as f:
-    #     f.write(str(filtered_lis))
 
 # コマンドライン引数を処理する
 if __name__ == '__main__':
-    args = sys.argv
-    if len(sys.argv) < 2:
+
+    if not file_path:
         sys.exit('Arguments are too short')
     else:
-        file_path = sys.argv[1]
         # 絶対パスへ変換
         path = os.path.abspath(file_path)
         # ファイルの存在有無
@@ -43,10 +60,5 @@ if __name__ == '__main__':
         # .docxを想定している場合
         if path.split(".")[-1] != "docx":
             sys.exit("That's not the right extension!!!")
-        search_word = sys.argv[2]
+        search_word = cmd_args.word
         wordgrep(path, search_word)
-
-
-# python wordgrep.py "C:\Users\libso23-user\OneDrive - The University of Tokyo\ドキュメント\20221031要求水準書ひな形案.docx" 臭い
-
-
